3b91a9d3a2822b32a334f314fde15db380161ddd
Robert Evanik <rob.evanik@gmail.com>
2022-01-20 22:37:56 -0500

    Init sequelize

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on PORT: ${PORT}`));
+});
