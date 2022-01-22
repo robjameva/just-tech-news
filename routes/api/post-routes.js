c9e2e058f0534be7005e1bedea76130bcce935f7
Robert Evanik <rob.evanik@gmail.com>
2022-01-22 08:19:02 -0500

    created vote route and functionality

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
+
 
 // get all posts
 router.get('/', (req, res) => {
@@ -62,6 +64,35 @@ router.post('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+        .then(() => {
+            return Post.findOne({
+                where: {
+                    id: req.body.post_id
+                },
+                attributes: [
+                    'id',
+                    'post_url',
+                    'title',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                    ]
+                ]
+            })
+        })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => res.json(err));
+
+});
+
 
 router.put('/:id', (req, res) => {
     Post.update(
