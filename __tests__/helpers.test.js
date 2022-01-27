a3d6c4eda5dd4296d97fb42861d83310f1038da7
Robert Evanik <rob.evanik@gmail.com>
2022-01-26 21:57:24 -0500

    Added tests

---
+++ __tests__/helpers.test.js
@@ -0,0 +1,13 @@
+const { format_date, format_plural } = require('../utils/helpers');
+
+test('format_date() returns a date string', () => {
+    const date = new Date('2020-03-20 16:12:03');
+
+    expect(format_date(date)).toBe('3/20/2020');
+});
+
+test('format_plural() correctly pluralizes words', () => {
+
+    expect(format_plural('tiger', 2)).toBe('tigers');
+    expect(format_plural('lion', 1)).toBe('lion');
+})
